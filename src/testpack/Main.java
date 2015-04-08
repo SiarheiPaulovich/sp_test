915772e6722d3514081b3d15e53f05fb14cdf7a1
unknown <Siarhei_Paulovich@EPBYMINW5110.minsk.epam.com>
2015-04-08 16:55:59 +0300

    Added a test class

---
+++ src/testpack/Main.java
@@ -0,0 +1,16 @@
+package testpack;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 10; i++){
+			System.out.printf("%d ",i);
+		}
+		System.out.println();
+	}
+
+}
